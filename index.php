1ae168e00354111af6739e0791057d5968615182
Rob Fong <robert.fongii@outlook.com>
2020-02-21 06:40:52 -0700

    migrate rbfdev

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+    <title>RBFDev Media Queries</title>
+    <link rel="stylesheet" href="./css/rbfdev.css">
+</head>
+
+<body>
+    <header>
+        <h1>Responsive Web Design / UI/UX Designer</h1>
+        <subHeading>Open-Stack Developer / Freelancer Website Developer</subHeading>
+    </header>
+    <main>
+        <h2 class="about-me">TEST</h2>
+        <h3>Let's Connect</h3>
+        <div class="lets-connect">
+            <ul>
+                <li>Name: Rob Fong</li>
+                <li>Location: Glendale, Arizona</li>
+                <li>Skype: rob.fongii</li>
+                <li>Phone: (810) 471-2234</li>
+                <li>Email: robert.fongii@outlook.com</li>
+            </ul>
+        </div>
+            <h3>Meet Rob</h3>
+            <p>My passion is Website Development and Web Design with computer hardware and networking as welcomed distractions. I have had a love of computers since I was a teen. I began developing websites in the mid-90s and built my first computer in 1997.</p>
+            <p>I enjoy challenging website development projects. I am often asked “Can you do this?” and most often my reply is “Given enough time and money, YES!”  The Internet is a canvas that never ends and everyday day brings new challenges. I have been blessed with 2 sons that both enjoy computer programming. It seems to run in the family.</p>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </main>
+    <footer>
+        <?php echo "Hello World"; ?>
+    </footer>
+</body>
+
+</html>
