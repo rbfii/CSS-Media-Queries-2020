7617243bc308238c0442339049ebf07c4b71e109
Rob Fong <robert.fongii@outlook.com>
2020-02-21 06:44:27 -0700

    switching dev env

--- index.php
+++ index.php
@@ -25,9 +25,9 @@
                 <li>Email: robert.fongii@outlook.com</li>
             </ul>
         </div>
-            <h3>Meet Rob</h3>
-            <p>My passion is Website Development and Web Design with computer hardware and networking as welcomed distractions. I have had a love of computers since I was a teen. I began developing websites in the mid-90s and built my first computer in 1997.</p>
-            <p>I enjoy challenging website development projects. I am often asked “Can you do this?” and most often my reply is “Given enough time and money, YES!”  The Internet is a canvas that never ends and everyday day brings new challenges. I have been blessed with 2 sons that both enjoy computer programming. It seems to run in the family.</p>
+        <h3>Meet Rob</h3>
+        <p>My passion is Website Development and Web Design with computer hardware and networking as welcomed distractions. I have had a love of computers since I was a teen. I began developing websites in the mid-90s and built my first computer in 1997.</p>
+        <p>I enjoy challenging website development projects. I am often asked “Can you do this?” and most often my reply is “Given enough time and money, YES!” The Internet is a canvas that never ends and everyday day brings new challenges. I have been blessed with 2 sons that both enjoy computer programming. It seems to run in the family.</p>
         <div></div>
         <div></div>
         <div></div>
